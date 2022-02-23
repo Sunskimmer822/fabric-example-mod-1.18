5b186b0051d1411d898fb149985f849d83e8e7bb
Sunskimmer822 <92886026+Sunskimmer822@users.noreply.github.com>
2022-02-23 12:01:18 -0800

    Make portal work

--- src/main/java/net/test/mod/TestMod.java
+++ src/main/java/net/test/mod/TestMod.java
@@ -2,10 +2,13 @@ package net.test.mod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -20,7 +23,12 @@ public class TestMod implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("testmod", "fabric_item"), FABRIC_ITEM);
         Registry.register(Registry.BLOCK, new Identifier("testmod", "fabric_block"), FABRIC_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("testmod", "fabric_block"), new BlockItem(FABRIC_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
-        
+        CustomPortalBuilder.beginPortal()  
+.frameBlock(Blocks.DIAMOND_BLOCK)  
+.lightWithItem(Items.ENDER_EYE)  
+.destDimID(new Identifier("the_end"))  
+.tintColor(45,65,101)  
+.registerPortal();
     }
-
+    
 }
